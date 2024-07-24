f37966d566dcfc2ca1315502d01ec0787853d265
BABAK CHALAKI <168085103+Babak-Chalacki@users.noreply.github.com>
2024-07-24 10:30:57 +0330

    json and structure

---
+++ slider.js
@@ -0,0 +1,6 @@
+let JsonSlider = [
+    {id:1, img: "https://picsum.photos/id/237/200/300", title: "1", desc: "1"},
+    {id:2, img: "https://picsum.photos/200/301", title: "2", desc: "2"},
+    {id:3, img: "https://picsum.photos/200/302", title: "3", desc: "3"},
+    {id:4, img: "https://picsum.photos/200/303", title: "4", desc: "4"},
+]   
